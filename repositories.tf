c57b213f1835a8407133a546dea2a69e380ff584
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-02 15:58:22 +0400

    -

--- repositories.tf
+++ repositories.tf
@@ -1,5 +1,5 @@
 module "oci_iac_gaia" {
-  source = "./modules"
+  source = "./module"
 
   repository_name        = "oci-iac-gaia"
   repository_description = "This is the IAC repository for OCI infrastructure codenamed GAIA"
