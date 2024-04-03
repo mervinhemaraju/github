df8abdc8af47954964a5a28b3bf8e42fca1e84bb
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-03 16:43:11 +0400

    - Added new repo for oci scripts - Allowed force pushes on module repo

--- repositories.tf
+++ repositories.tf
@@ -4,5 +4,14 @@ module "oci_iac_gaia" {
   repository_name        = "oci-iac-gaia"
   repository_description = "This is the IAC repository for OCI infrastructure codenamed GAIA"
 
+  has_wiki = true
+}
+
+module "oci_scripts" {
+  source = "./module"
+
+  repository_name        = "oci-scripts"
+  repository_description = "Scripts to facilitate management of resources via OCI"
+
   has_wiki = false
 }
