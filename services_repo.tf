f20c4dba9360ec85b69fbba623fd9b8f88f9fde2
Mervin Hemaraju <115166315+mervinhemaraju@users.noreply.github.com>
2024-05-04 17:50:21 +0400

    - Removed mongodb atlas repo (#11)

--- services_repo.tf
+++ services_repo.tf
@@ -6,12 +6,3 @@ module "cloudflare" {
 
   has_wiki = false
 }
-
-module "mongodb_atlas" {
-  source = "./module"
-
-  name        = "mongodb-atlas"
-  description = "The main IAC code for MongoDB Atlas"
-
-  has_wiki = false
-}
