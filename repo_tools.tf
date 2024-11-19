fb3db51db064b4fd391da74eaaca9d62aede98fa
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-11-19 10:51:47 +0400

    - Repo cleaning - Added new repo for okta jit - Made hawkeye repo public

---
+++ repo_tools.tf
@@ -0,0 +1,21 @@
+module "hawkeye" {
+  source = "./module"
+
+  name        = "hawkeye"
+  description = "Hawkeye is an event monitoring tool that captures records about specific AWS roles on AWS and then sends them in real-time on a Slack Channel."
+
+  enable_branch_protection = false
+
+  has_wiki = false
+}
+
+module "okta_just_in_time" {
+  source = "./module"
+
+  name        = "jit-tool-aws"
+  description = "A JIT tool that will allow just in time access to okta groups."
+
+  enable_branch_protection = false
+
+  has_wiki = false
+}
