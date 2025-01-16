2203636ccc7466c0561af7ac68faa624c0570690
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-01-17 03:28:58 +0400

    - Added new repo glamu - Made mauritius emergency services private

--- repo_mobile_apps.tf
+++ repo_mobile_apps.tf
@@ -7,15 +7,6 @@ module "mes" {
   has_wiki = false
 }
 
-module "mauritius_emergency_services" {
-  source = "./module"
-
-  name        = "mauritius-emergency-services"
-  description = "This is the main Github Repo of M.E.S (Mauritius Emergency Services), an Android application that has a list of all emergency phone services available in Mauritius."
-
-  has_wiki = false
-}
-
 module "locky" {
   source = "./module"
 
@@ -23,4 +14,27 @@ module "locky" {
   description = "This is the official Github Repo of Locky, an offline Material Design Password Manager for Android"
 
   has_wiki = false
+}
+
+
+module "mauritius_emergency_services" {
+  source = "./module"
+
+  name        = "mauritius-emergency-services"
+  description = "This is the main Github Repo of M.E.S (Mauritius Emergency Services), a mobile application for Android and IOS, that provides information about emergency services in Mauritius."
+
+  has_wiki = false
+
+  enable_branch_protection = false
+}
+
+module "glamu" {
+  source = "./module"
+
+  name        = "glamu"
+  description = "GlamU is a mobile application for Android and IOS, that allows booking appointments for beauty and fashion in Mauritius."
+
+  has_wiki = false
+
+  enable_branch_protection = false
 }
