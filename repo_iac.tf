dd660c6444defc147c3c145078001efab8f166db
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-07-25 17:43:02 +0400

    - Added a repo for auth0

--- repo_iac.tf
+++ repo_iac.tf
@@ -12,12 +12,20 @@ module "glamu_iac" {
   has_wiki = false
 }
 
-
 module "vercel_iac" {
   source = "./module"
 
   name        = "vercel-iac"
   description = "This is the main IaC repository for the application Vercel."
+
+  has_wiki = false
+}
+
+module "auth0_iac" {
+  source = "./module"
+
+  name        = "auth0-iac"
+  description = "This is the main IaC repository for the application Auth0."
 
   has_wiki = false
 }
