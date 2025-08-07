06d32684f3ceae486af295ee75cb90b0f3df7f44
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-07 22:26:29 +0400

    - Deleted launcher repo in mervinhemaraju

--- components/mervinhemaraju/repo_scripts.tf
+++ components/mervinhemaraju/repo_scripts.tf
@@ -15,22 +15,3 @@ module "aws_iam_scanner" {
 
   has_wiki = false
 }
-
-module "launcher" {
-  source = "../../module"
-
-  name        = "launcher"
-  description = "Launcher is a repo that holds code for all launcher scripts and tools that I use to quickly launch workflows on my resources."
-
-  enable_branch_protection = true
-
-  environment_secrets = {
-    production = {
-      secrets = {
-        "DOPPLER_TOKEN" = var.token_doppler_global
-      }
-    }
-  }
-
-  has_wiki = false
-}
