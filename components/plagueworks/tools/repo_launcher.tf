4f92d8e4614378a950e389071c4ce2c3025d3250
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-07 22:10:33 +0400

    - Added new repo in plagueworks tool Launcher

---
+++ components/plagueworks/tools/repo_launcher.tf
@@ -0,0 +1,18 @@
+module "launcher" {
+  source = "../../module"
+
+  name        = "launcher"
+  description = "Launcher is a repo that holds code for all scripts and tools that I use to quickly launch workflows on my resources."
+
+  enable_branch_protection = true
+
+  environment_secrets = {
+    production = {
+      secrets = {
+        "DOPPLER_TOKEN" = var.token_doppler_global
+      }
+    }
+  }
+
+  has_wiki = false
+}
