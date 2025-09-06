d9c11f9bfbc71d2235309be4128989d4c535667e
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-09-06 17:25:48 +0400

    - Fixed module reference

--- components/plagueworks/apps/repo_lockme.tf
+++ components/plagueworks/apps/repo_lockme.tf
@@ -1,6 +1,6 @@
 # Repository for the lockme website
 module "lockme_website" {
-  source = "../../module"
+  source = "../../../module"
 
   name        = "lockme-website"
   description = "The Lockme website repo that is a web application that provides information about the Lockme Mobile Application."
