198616686057cb4df571ae010f3f31eff9b33a77
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-09-16 16:12:46 +0400

    - Fixed konekte repo template location

--- components/plagueworks/apps/repo_konekte.tf
+++ components/plagueworks/apps/repo_konekte.tf
@@ -8,7 +8,7 @@ module "konekte" {
   description = "Konekte is a mobile application for Android and IOS, that allows users to connect with each other in Mauritius."
   visibility  = "private"
 
-  from_template = "mervinhemaraju/template-mobile-flutter"
+  from_template = "template-mobile-flutter"
 
   enable_branch_protection = false
   has_wiki                 = false
