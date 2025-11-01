87060dd00ceb9c0925d0a288aaa67681b57e7aea
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-11-02 00:59:29 +0400

    - Added a new repository for custom runner images

--- components/mervinhemaraju/repo_others.tf
+++ components/mervinhemaraju/repo_others.tf
@@ -16,3 +16,12 @@ module "mervinhemaraju" {
 
   has_wiki = false
 }
+
+module "runner_images" {
+  source = "../../module"
+
+  name        = "runner-images"
+  description = "Repository that contains custom runner images for all my tools and frameworks."
+
+  has_wiki = false
+}
